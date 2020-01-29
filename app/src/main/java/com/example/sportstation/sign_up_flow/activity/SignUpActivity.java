973738d7f01d1a110238934347873ada5130916e
DmitryFilimonov1993 <Ddrogba11drogba11>
2020-01-29 21:48:09 +0300

    StartProject

---
+++ app/src/main/java/com/example/sportstation/sign_up_flow/activity/SignUpActivity.java
@@ -0,0 +1,6 @@
+package com.example.sportstation.sign_up_flow.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/sportstation/sign_up_flow/fragments/CreateSoftAccountFragment.java
@@ -0,0 +1,6 @@
+package com.example.sportstation.sign_up_flow.fragments;
+
+import androidx.fragment.app.Fragment;
+
+public class CreateSoftAccountFragment extends Fragment {
+}
