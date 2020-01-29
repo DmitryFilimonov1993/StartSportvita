b93cea8652830c2ada76f5441a4e90c3daeec6f5
DmitryFilimonov1993 <Ddrogba11drogba11>
2020-01-30 02:43:51 +0300

    Created navController

--- app/src/main/java/com/example/sportstation/sign_up_flow/activity/SignUpActivity.java
+++ app/src/main/java/com/example/sportstation/sign_up_flow/activity/SignUpActivity.java
@@ -1,6 +1,22 @@
 package com.example.sportstation.sign_up_flow.activity;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.example.sportstation.R;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        navController = Navigation.findNavController(this,R.id.nav_host_fragment_container);
+    }
 }
